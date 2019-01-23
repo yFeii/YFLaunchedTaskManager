7816c5d5ac6c21d048652fbfa81e0fbbb46071a0
git <15524589871@163.com>
2019-01-23 11:46:03 +0800

    clean

--- Example/Pods/Target Support Files/Pods-YFLaunchedTaskManager_Example/Pods-YFLaunchedTaskManager_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_YFLaunchedTaskManager_Example : NSObject
-@end
-@implementation PodsDummy_Pods_YFLaunchedTaskManager_Example
-@end
--- Example/Pods/Target Support Files/YFLaunchedTaskManager/YFLaunchedTaskManager-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_YFLaunchedTaskManager : NSObject
-@end
-@implementation PodsDummy_YFLaunchedTaskManager
-@end
